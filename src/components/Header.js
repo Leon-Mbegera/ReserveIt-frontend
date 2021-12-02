eecfee92145cb2f29167faca056bd06a6cfe8241
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-02 17:14:23 +0300

    Create header element to host navigation elements

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  <header>
+    <nav>
+      <div>
+        <Link to="/signup">sign up</Link>
+        <Link to="/signin">sign up</Link>
+      </div>
+    </nav>
+  </header>;
+};
+
+export default Header;
