bb59e7c6d269ccc799ce49ab63b0c00a780d6c72
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-03 11:52:02 +0300

    Create a route to the user sign in page

---
+++ src/components/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import SignIn from './SignIn';
+
+const Router = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route exact path="/signin" element={SignIn} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,12 +4,20 @@ const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <form>
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" value="" />
+      <input type="email" id="email" value="" onChange={(e) => handleEmail(e.target.value)} />
       <label htmlFor="password">Password</label>
-      <input type="password" id="password" value="" />
+      <input type="password" id="password" value="" onChange={(e) => handlePassword(e.target.value)} />
       <button type="submit">Sign in</button>
     </form>
   );
