8dbe2a061c2ae0f7fd3895d450a6940ba73be72f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-20 11:51:49 +0300

    Fetch and render a single car instance on UI

--- src/containers/SingleCar.js
+++ src/containers/SingleCar.js
@@ -1,31 +1,31 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchAllCars } from '../actions/index';
+import { useParams } from 'react-router-dom';
+import { fetchEachCar } from '../actions/index';
 
-const AllCars = () => {
-  const { cars } = useSelector((state) => state);
+const SingleCar = () => {
+  const { car } = useSelector((state) => state);
   const dispatch = useDispatch();
+  const { id } = useParams();
 
   useEffect(() => {
     dispatch(
-      fetchAllCars(),
+      fetchEachCar(id),
     );
   }, []);
 
   return (
-    <main>
-      {cars.data.map((car, idx) => (
+    <div>
+      {car.data.map((car) => (
         <div key={`car-${car.id}`}>
           <img src={car.image} alt="car" />
           <p>{car.model}</p>
           <p>{car.mileage}</p>
           <p>{car.price}</p>
-          <Link to={`/cars/${idx}`}>view</Link>
         </div>
       ))}
-    </main>
+    </div>
   );
 };
 
-export default AllCars;
+export default SingleCar;
