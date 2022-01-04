f4e10c1cdae3c8077157691b18f32b33aaa17c3d
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-04 18:08:41 +0300

    Create delete action to remove reservation

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { GetMyReservations } from '../containers/APIs';
 
 const MyReservations = () => {
   const [myReserves, setMyReserves] = useState('');
+  const navigate = useNavigate();
+
   useEffect(() => {
     GetMyReservations()
       .then((response) => {
         setMyReserves(response);
       });
   }, []);
+
+  const handleDelete = (id) => {
+    const accessToken = localStorage.getItem('accessToken');
+    fetch(`http://localhost:3000/reservations/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }).then(() => {
+      navigate('/MyReservations');
+    });
+  };
 
   return (
     <>
@@ -21,6 +37,7 @@ const MyReservations = () => {
             <p>{each.agreement}</p>
             <p>{each.city}</p>
             <p>{each.date}</p>
+            <button type="submit" onClick={() => handleDelete(each.car.id)}>delete</button>
           </div>
         ))
       ) : <p>Just a sec...</p>}
