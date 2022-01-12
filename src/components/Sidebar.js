7aa4e756698fa325686057f42635129d87d13e74
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-12 15:18:54 +0300

    Show either only menu bars or menu items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,36 +3,56 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import '../index.css';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import '../App.css';
 
 const Sidebar = ({ navigationLinks }) => {
   const [currentRoute, setCurrentRoute] = useState('Home');
+  const [sideBar, setSidebar] = useState(false);
 
   return (
-    <nav className="fixed left-2 top-0 bottom-0 z-50 w-40 pt-20 flex flex-col h-screen items-center border-r-2 border-light-grey">
-      <ul className="list-none">
-        {navigationLinks.map((element) => (
-          <li
-            key={element.type}
-            className={classNames([
-              'text-dark-grey p-2',
-              currentRoute === element && 'text-white bg-green px-2',
-            ])}
-          >
-            <button
-              type="submit"
-              onClick={() => setCurrentRoute(element)}
+    <>
+      {!sideBar && (
+      <div className="navbar">
+        <FaBars
+          className="menu-bars"
+          onClick={() => setSidebar(!sideBar)}
+        />
+      </div>
+      )}
+      <nav className={sideBar ? 'fixed left-2 top-0 bottom-0 w-40 flex flex-col h-screen items-center border-r-2 border-light-grey nav-menu active' : 'nav-menu deactive'}>
+        <div className="navbar-toggler">
+          <AiOutlineClose
+            className="menu-close"
+            onClick={() => setSidebar(!sideBar)}
+          />
+        </div>
+        <ul className="list-none pt-20">
+          {navigationLinks.map((element) => (
+            <li
+              key={element.type}
+              className={classNames([
+                'text-dark-grey p-2',
+                currentRoute === element && 'text-white bg-green px-2',
+              ])}
             >
-              <Link
-                to={`/${element}`}
-                className="font-bold"
+              <button
+                type="submit"
+                onClick={() => setCurrentRoute(element)}
               >
-                {element.toUpperCase()}
-              </Link>
-            </button>
-          </li>
-        ))}
-      </ul>
-    </nav>
+                <Link
+                  to={`/${element}`}
+                  className="font-bold"
+                >
+                  {element.toUpperCase()}
+                </Link>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 };
 
