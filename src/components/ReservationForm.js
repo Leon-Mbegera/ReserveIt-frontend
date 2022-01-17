d859a89142c235f86c1263ba3cf055c8cdb7f1b8
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-17 13:56:41 +0300

    Style and position the make reservation button

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -65,7 +65,7 @@ const ReservationForm = ({ car, user, UpdateReservations }) => {
                 <label htmlFor="model" className="block text-gray-500 font-bold md:text-left mb-1 md:mb-1 pr-4 text-left">Select date:</label>
                 <input type="date" required onChange={(e) => setDate(e.target.value)} className="appearance-none w-full bg-gray-200 text-gray-700 rounded p-2 mb-3 leading-tight border focus:border-red-500 focus:bg-white focus:outline-none" />
               </div>
-              { !isPending && <button type="submit">Make Reservation</button> }
+              { !isPending && <button type="submit" className="bg-green hover:bg-blue-700 text-white font-bold p-2 rounded h-10 align-middle self-center">Make Reservation</button> }
               { isPending && <button type="button" disabled>Making Reservation ...</button> }
             </div>
           </div>
