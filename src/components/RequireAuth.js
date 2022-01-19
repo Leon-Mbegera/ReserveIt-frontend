14b5513d17a3e3b937c171b1c8c3a9e583f6c375
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-19 18:39:12 +0300

    Create and export RequireAuth that checks if user is authorized to access a particular route

---
+++ src/components/RequireAuth.js
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const RequireAuth = () => {
+  const { auth } = useAuth();
+  // const location = useLocation();
+
+  return (
+    auth.accessToken ? <Outlet /> : <Navigate to="/signin" />
+  );
+};
+
+export default RequireAuth;
