28b75fe6dca043129d7c33bc63de2e732d775368
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-27 17:54:30 +0300

    Dispatch an action to reset the user state in the redux store to null upon sign out

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,11 +1,18 @@
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import navigation from '../data/navigation';
+import { resetCurrentUser } from '../actions/index';
 
 const SignOut = () => {
+  const dispatch = useDispatch();
+  navigation();
+
   const setAuthorized = useAuth()[1];
   useEffect(() => {
     setAuthorized(null);
+    dispatch(resetCurrentUser(null));
   }, []);
 
   return (
